4ae1107328750b40cd58fdc5a83d769066ddf825
johnmeshulam <johnmeshulam@gmail.com>
2019-11-05 12:51:53 +0200

    remove samples

--- src/user/HardwareCreator.java
+++ src/user/HardwareCreator.java
@@ -1,14 +1,7 @@
 package user;
 
-import robot.hardware.chassis.RobotChassis;
-import robot.hardware.motors.MediumMotor;
-import robot.hardware.sensors.GyroSensor;
-
 public class HardwareCreator {
 	public static void init() {
-		new RobotChassis("leftMotor", 'B', "rightMotor", 'C');
-		new MediumMotor("armMotor", 'A');
-		
-		new GyroSensor("gyro", 1);
+			//init your hardware here
 	}
 }
--- src/user/RunCreator.java
+++ src/user/RunCreator.java
@@ -1,11 +1,7 @@
 package user;
 
-import user.runs.Run1;
-import user.runs.Run2;
-
 public class RunCreator {
 	public static void init() {
-		new Run1("my first run");
-		new Run2("my second run");
+		//init your runs here
 	}
 }
